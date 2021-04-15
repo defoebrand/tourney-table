428c0526eb773457473a0256fe543463bfa0ac65
Brandon <defoe.brand@gmail.com>
2021-04-15 12:29:46 +0300

    Add linters and adjust files for linter errors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from './containers/App';
 
-import './assets/index.css';
+import './assets/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
